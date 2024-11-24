e729a201bad1bfd86c0909db7b88008337499bed
Nakun Santishurntornkul <109501622+6588141Nagun@users.noreply.github.com>
2024-11-24 09:58:30 +0000

    Test Case

--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,15 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-5, -3), -8) 
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2) 
+        self.assertEqual(self.calc.subtract(-5, -3), -2)  
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)  
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)  
