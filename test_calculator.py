5d4737e424ab0098792ca7e749d09d4dc84d341c
Nakun Santishurntornkul <109501622+6588141Nagun@users.noreply.github.com>
2024-11-24 09:57:20 +0000

    testcase

--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,13 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
-    # Add the following test methods to the TestCalculator class:
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)  
+        self.assertEqual(self.calc.divide(9, 3), 3) 
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1) 
+        self.assertEqual(self.calc.modulo(14, 5), 4)  
 
 if __name__ == '__main__':
     unittest.main()
